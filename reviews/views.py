61298e8f71c6e6d9e58fd73169d6f4c757f5cfea
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-03-22 06:14:25 +0300

    The ListView

--- reviews/views.py
+++ reviews/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from .forms import ReviewForm
 from .models import Review
@@ -26,7 +27,6 @@ class ReviewView(View):
     return render(request, "reviews/review.html", {
       "form": form
     })
-
 
 
 # def review(request):
@@ -65,10 +65,10 @@ class ReviewView(View):
 #   return render(request, "reviews/thank_you.html")
 
 
-
 # class ThankYouView(View):
 #   def get(self, request):
 #     return render(request, "reviews/thank_you.html")
+
 
 class ThankYouView(TemplateView):
   template_name = "reviews/thank_you.html"
@@ -79,14 +79,25 @@ class ThankYouView(TemplateView):
     return context
 
 
-class ReviewListView(TemplateView):
-  template_name = "reviews/review_list.html"
+# class ReviewListView(TemplateView):
+#   template_name = "reviews/review_list.html"
+#
+#   def get_context_data(self, **kwargs):
+#     context = super().get_context_data(**kwargs)
+#     reviews = Review.objects.all()
+#     context["reviews"] = reviews
+#     return context
 
-  def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    reviews = Review.objects.all()
-    context["reviews"] = reviews
-    return context
+
+class ReviewListView(ListView):
+  template_name = "reviews/review_list.html"
+  model = Review
+  context_object_name = "reviews"
+
+  # def get_queryset(self):
+  #   base_query = super().get_queryset()
+  #   data = base_query.filter(rating__gt=4)  # > 4
+  #   return data
 
 
 class SingleReviewView(TemplateView):
